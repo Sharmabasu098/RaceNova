2bc79d9bc525ffd7af6541becd3300189db56ee0
Basudev sharma <147743632+Sharmabasu098@users.noreply.github.com>
2026-07-30 20:56:44 +0530

    Update RendererManager.js

--- src/core/renderer/RendererManager.js
+++ src/core/renderer/RendererManager.js
@@ -1,1 +1,52 @@
+/**
+ * ============================================================
+ * RaceNova
+ * Renderer Manager
+ * ============================================================
+ */
 
+import * as THREE from "three";
+
+export default class RendererManager {
+
+    constructor() {
+
+        this.renderer = new THREE.WebGLRenderer({
+            antialias: true
+        });
+
+        this.renderer.setSize(
+            window.innerWidth,
+            window.innerHeight
+        );
+
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+
+        this.renderer.shadowMap.enabled = true;
+
+    }
+
+    getRenderer() {
+        return this.renderer;
+    }
+
+    appendTo(container = document.body) {
+        container.appendChild(this.renderer.domElement);
+    }
+
+    resize() {
+
+        this.renderer.setSize(
+            window.innerWidth,
+            window.innerHeight
+        );
+
+    }
+
+    render(scene, camera) {
+
+        this.renderer.render(scene, camera);
+
+    }
+
+}
