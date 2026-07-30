5a91cd63e8f8bc6b008defc262754e31b082dfd0
Basudev sharma <147743632+Sharmabasu098@users.noreply.github.com>
2026-07-30 21:12:23 +0530

    Update Road.js

--- src/world/Road.js
+++ src/world/Road.js
@@ -1,1 +1,48 @@
+/**
+ * ============================================================
+ * RaceNova
+ * Road
+ * ============================================================
+ */
 
+import * as THREE from "three";
+import { WORLD } from "../constants/worldConstants.js";
+
+export default class Road {
+
+    constructor() {
+
+        this.mesh = this.createRoad();
+
+    }
+
+    createRoad() {
+
+        const geometry = new THREE.PlaneGeometry(
+            WORLD.ROAD_WIDTH,
+            WORLD.ROAD_LENGTH
+        );
+
+        const material = new THREE.MeshStandardMaterial({
+            color: 0x2d2d2d
+        });
+
+        const road = new THREE.Mesh(
+            geometry,
+            material
+        );
+
+        road.rotation.x = -Math.PI / 2;
+        road.receiveShadow = true;
+
+        return road;
+
+    }
+
+    getMesh() {
+
+        return this.mesh;
+
+    }
+
+}
