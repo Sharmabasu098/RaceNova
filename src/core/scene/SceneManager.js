6259192dedb9c9c6667bc5c22b509de1acb6fb1f
Basudev sharma <147743632+Sharmabasu098@users.noreply.github.com>
2026-07-30 20:50:23 +0530

    Update SceneManager.js

--- src/core/scene/SceneManager.js
+++ src/core/scene/SceneManager.js
@@ -1,1 +1,32 @@
+/**
+ * ============================================================
+ * RaceNova
+ * Scene Manager
+ * ============================================================
+ */
 
+import * as THREE from "three";
+
+export default class SceneManager {
+
+    constructor() {
+
+        this.scene = new THREE.Scene();
+
+        this.scene.background = new THREE.Color(0x87CEEB);
+
+    }
+
+    getScene() {
+
+        return this.scene;
+
+    }
+
+    setBackground(color) {
+
+        this.scene.background = new THREE.Color(color);
+
+    }
+
+}
