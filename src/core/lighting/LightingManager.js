c9ab7378da3e410cda5c73ba08cd9442954b1940
Basudev sharma <147743632+Sharmabasu098@users.noreply.github.com>
2026-07-30 21:00:00 +0530

    Update LightingManager.js

--- src/core/lighting/LightingManager.js
+++ src/core/lighting/LightingManager.js
@@ -1,1 +1,47 @@
+/**
+ * ============================================================
+ * RaceNova
+ * Lighting Manager
+ * ============================================================
+ */
 
+import * as THREE from "three";
+
+export default class LightingManager {
+
+    constructor(scene) {
+
+        this.scene = scene;
+
+        this.createAmbientLight();
+        this.createDirectionalLight();
+
+    }
+
+    createAmbientLight() {
+
+        this.ambientLight = new THREE.AmbientLight(
+            0xffffff,
+            1.0
+        );
+
+        this.scene.add(this.ambientLight);
+
+    }
+
+    createDirectionalLight() {
+
+        this.sunLight = new THREE.DirectionalLight(
+            0xffffff,
+            2.2
+        );
+
+        this.sunLight.position.set(20, 30, 10);
+
+        this.sunLight.castShadow = true;
+
+        this.scene.add(this.sunLight);
+
+    }
+
+}
