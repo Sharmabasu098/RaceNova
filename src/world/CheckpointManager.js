01e55fdbd2671b9b9720ecb7c43722dc8824f1fa
Basudev sharma <147743632+Sharmabasu098@users.noreply.github.com>
2026-07-30 21:25:07 +0530

    Update CheckpointManager.js

--- src/world/CheckpointManager.js
+++ src/world/CheckpointManager.js
@@ -1,1 +1,66 @@
+/**
+ * ============================================================
+ * RaceNova
+ * Checkpoint Manager
+ * ============================================================
+ */
 
+import * as THREE from "three";
+
+export default class CheckpointManager {
+
+    constructor(scene) {
+
+        this.scene = scene;
+        this.checkpoints = [];
+
+    }
+
+    createCheckpoint(x, y, z) {
+
+        const geometry = new THREE.BoxGeometry(
+            8,
+            5,
+            1
+        );
+
+        const material = new THREE.MeshStandardMaterial({
+            color: 0x00ff00,
+            transparent: true,
+            opacity: 0.35
+        });
+
+        const checkpoint = new THREE.Mesh(
+            geometry,
+            material
+        );
+
+        checkpoint.position.set(x, y, z);
+
+        this.scene.add(checkpoint);
+
+        this.checkpoints.push(checkpoint);
+
+        return checkpoint;
+
+    }
+
+    getCheckpoints() {
+
+        return this.checkpoints;
+
+    }
+
+    clear() {
+
+        this.checkpoints.forEach(checkpoint => {
+
+            this.scene.remove(checkpoint);
+
+        });
+
+        this.checkpoints = [];
+
+    }
+
+}
