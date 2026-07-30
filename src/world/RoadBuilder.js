11af9c4208658e6e771c519e63d4f12d72c5d21b
Basudev sharma <147743632+Sharmabasu098@users.noreply.github.com>
2026-07-30 21:15:27 +0530

    Update RoadBuilder.js

--- src/world/RoadBuilder.js
+++ src/world/RoadBuilder.js
@@ -1,1 +1,55 @@
+/**
+ * ============================================================
+ * RaceNova
+ * Road Builder
+ * ============================================================
+ */
 
+import * as THREE from "three";
+import Road from "./Road.js";
+
+export default class RoadBuilder {
+
+    constructor(scene) {
+
+        this.scene = scene;
+        this.road = null;
+
+    }
+
+    build() {
+
+        this.road = new Road();
+
+        this.scene.add(
+            this.road.getMesh()
+        );
+
+    }
+
+    getRoad() {
+
+        return this.road;
+
+    }
+
+    remove() {
+
+        if (!this.road) return;
+
+        this.scene.remove(
+            this.road.getMesh()
+        );
+
+        this.road = null;
+
+    }
+
+    rebuild() {
+
+        this.remove();
+        this.build();
+
+    }
+
+}
