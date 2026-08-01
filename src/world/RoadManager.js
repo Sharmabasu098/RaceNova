cf597730a76fd60d45642889d175fdbe77aae4c6
Basudev sharma <147743632+Sharmabasu098@users.noreply.github.com>
2026-08-01 19:57:47 +0530

    Update RoadManager.js

--- src/world/RoadManager.js
+++ src/world/RoadManager.js
@@ -1,1 +1,57 @@
+/**
+ * ============================================================
+ * RaceNova
+ * Road Manager
+ * Version : 1.0
+ * ============================================================
+ */
 
+export default class RoadManager {
+
+    constructor(roadBuilder, roadMarkings, roadBarrier) {
+
+        this.roadBuilder = roadBuilder;
+        this.roadMarkings = roadMarkings;
+        this.roadBarrier = roadBarrier;
+
+        this.speed = 0.0;
+        this.maxSpeed = 0.35;
+
+    }
+
+    start() {
+
+        this.speed = 0.08;
+
+    }
+
+    stop() {
+
+        this.speed = 0;
+
+    }
+
+    update(deltaTime) {
+
+        // अगले Version में
+        // Endless Road
+        // Speed
+        // Nitro
+        // Traffic
+        // सब यहीं आएगा
+
+    }
+
+    getSpeed() {
+
+        return this.speed;
+
+    }
+
+    setSpeed(speed) {
+
+        this.speed = Math.min(speed, this.maxSpeed);
+
+    }
+
+}
