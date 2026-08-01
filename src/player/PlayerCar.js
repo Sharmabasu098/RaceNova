0b391a1b0eb7e7f629fd8507e336c4291a9c5384
Basudev sharma <147743632+Sharmabasu098@users.noreply.github.com>
2026-08-01 18:56:27 +0530

    Update PlayerCar.js

--- src/player/PlayerCar.js
+++ src/player/PlayerCar.js
@@ -1,1 +1,33 @@
+import * as THREE from "three";
 
+export default class PlayerCar {
+
+    constructor(scene) {
+
+        this.scene = scene;
+
+        const geometry = new THREE.BoxGeometry(1.2, 0.6, 2.4);
+
+        const material = new THREE.MeshStandardMaterial({
+            color: 0xff0000
+        });
+
+        this.mesh = new THREE.Mesh(geometry, material);
+
+        this.mesh.position.set(0, 0.4, 5);
+
+        this.mesh.castShadow = true;
+
+        this.scene.add(this.mesh);
+
+    }
+
+    update(deltaTime) {
+        // आगे यहीं कार की मूवमेंट जोड़ेंगे
+    }
+
+    getMesh() {
+        return this.mesh;
+    }
+
+}
