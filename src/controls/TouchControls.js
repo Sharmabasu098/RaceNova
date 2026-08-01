1efa6ded377f9e220c352aaba5a7747169260f07
Basudev sharma <147743632+Sharmabasu098@users.noreply.github.com>
2026-08-01 19:24:16 +0530

    Update TouchControls.js

--- src/controls/TouchControls.js
+++ src/controls/TouchControls.js
@@ -1,1 +1,40 @@
+/**
+ * ====================================
+ * RaceNova
+ * Touch Controls
+ * ====================================
+ */
 
+export default class TouchControls {
+
+    constructor(player) {
+
+        this.player = player;
+
+        this.moveSpeed = 0.15;
+
+        this.init();
+
+    }
+
+    init() {
+
+        window.addEventListener("touchstart", (event) => {
+
+            const x = event.touches[0].clientX;
+
+            if (x < window.innerWidth / 2) {
+
+                this.player.getMesh().position.x -= this.moveSpeed;
+
+            } else {
+
+                this.player.getMesh().position.x += this.moveSpeed;
+
+            }
+
+        });
+
+    }
+
+}
