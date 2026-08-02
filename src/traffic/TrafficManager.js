ee89f60e59bccd6a797573f0502eabde3cb7a304
Basudev sharma <147743632+Sharmabasu098@users.noreply.github.com>
2026-08-02 14:58:30 +0530

    Update TrafficManager.js

--- src/traffic/TrafficManager.js
+++ src/traffic/TrafficManager.js
@@ -1,1 +1,49 @@
+/**
+ * ============================================================
+ * RaceNova
+ * Traffic Manager
+ * Version : 1.0
+ * ============================================================
+ */
 
+import TrafficCar from "./TrafficCar.js";
+
+export default class TrafficManager {
+
+    constructor(scene) {
+
+        this.scene = scene;
+
+        this.cars = [];
+
+    }
+
+    spawnCar(lane, z) {
+
+        const car = new TrafficCar(
+            this.scene,
+            lane,
+            z
+        );
+
+        this.cars.push(car);
+
+    }
+
+    update(speed) {
+
+        this.cars.forEach((car) => {
+
+            car.update(speed);
+
+        });
+
+    }
+
+    getCars() {
+
+        return this.cars;
+
+    }
+
+}
