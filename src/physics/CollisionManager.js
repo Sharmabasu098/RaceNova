5f171f003acc17d415d1706e768da858f212540b
Basudev sharma <147743632+Sharmabasu098@users.noreply.github.com>
2026-08-02 15:49:05 +0530

    Update CollisionManager.js

--- src/physics/CollisionManager.js
+++ src/physics/CollisionManager.js
@@ -1,1 +1,63 @@
+/**
+ * ============================================================
+ * RaceNova
+ * Collision Manager
+ * Version : 1.0
+ * ============================================================
+ */
 
+export default class CollisionManager {
+
+    constructor(player, trafficManager) {
+
+        this.player = player;
+        this.trafficManager = trafficManager;
+
+        this.crashed = false;
+
+    }
+
+    update() {
+
+        if (this.crashed) return;
+
+        const playerMesh = this.player.getMesh();
+
+        const cars = this.trafficManager.getCars();
+
+        for (const traffic of cars) {
+
+            const trafficMesh = traffic.getMesh();
+
+            const distance =
+                playerMesh.position.distanceTo(
+                    trafficMesh.position
+                );
+
+            if (distance < 2.0) {
+
+                this.crashed = true;
+
+                console.log("💥 Collision Detected!");
+
+                break;
+
+            }
+
+        }
+
+    }
+
+    isCrashed() {
+
+        return this.crashed;
+
+    }
+
+    reset() {
+
+        this.crashed = false;
+
+    }
+
+}
