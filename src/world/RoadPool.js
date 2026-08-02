926fd7bd0e696c6d8594c46bc7b3412a727ead94
Basudev sharma <147743632+Sharmabasu098@users.noreply.github.com>
2026-08-02 06:44:45 +0530

    Update RoadPool.js

--- src/world/RoadPool.js
+++ src/world/RoadPool.js
@@ -1,1 +1,38 @@
+/**
+ * ============================================================
+ * RaceNova
+ * Road Pool
+ * ============================================================
+ */
 
+import RoadSegment from "./RoadSegment.js";
+import { WORLD } from "../constants/worldConstants.js";
+
+export default class RoadPool {
+
+    constructor(scene, count = 3) {
+
+        this.scene = scene;
+        this.segments = [];
+
+        for (let i = 0; i < count; i++) {
+
+            const segment = new RoadSegment(
+                -i * WORLD.ROAD_LENGTH
+            );
+
+            this.scene.add(segment.getMesh());
+
+            this.segments.push(segment);
+
+        }
+
+    }
+
+    getSegments() {
+
+        return this.segments;
+
+    }
+
+}
