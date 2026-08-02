5f48d86d1f31cdb40bb155d22d16e844b9ebd47c
Basudev sharma <147743632+Sharmabasu098@users.noreply.github.com>
2026-08-02 08:17:28 +0530

    Update PlayerMovement.js

--- src/player/PlayerMovement.js
+++ src/player/PlayerMovement.js
@@ -1,1 +1,57 @@
+/**
+ * ============================================================
+ * RaceNova
+ * Player Movement System
+ * Version : 1.0
+ * ============================================================
+ */
 
+export default class PlayerMovement {
+
+    constructor(player) {
+
+        this.player = player;
+
+        this.speed = 0;
+
+        this.acceleration = 0.08;
+
+        this.maxSpeed = 0.60;
+
+        this.brakeForce = 0.15;
+
+    }
+
+    update(deltaTime) {
+
+        if (this.speed < this.maxSpeed) {
+
+            this.speed += this.acceleration * deltaTime;
+
+        }
+
+        const mesh = this.player.getMesh();
+
+        mesh.position.z -= this.speed;
+
+    }
+
+    brake(deltaTime) {
+
+        this.speed -= this.brakeForce * deltaTime;
+
+        if (this.speed < 0) {
+
+            this.speed = 0;
+
+        }
+
+    }
+
+    getSpeed() {
+
+        return this.speed;
+
+    }
+
+}
