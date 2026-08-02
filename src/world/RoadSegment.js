9eaecfa36adef877887e2e8b9e8c4049a894492b
Basudev sharma <147743632+Sharmabasu098@users.noreply.github.com>
2026-08-02 06:43:05 +0530

    Update RoadSegment.js

--- src/world/RoadSegment.js
+++ src/world/RoadSegment.js
@@ -1,1 +1,68 @@
+/**
+ * ============================================================
+ * RaceNova
+ * Road Segment
+ * ============================================================
+ */
 
+import * as THREE from "three";
+import { WORLD } from "../constants/worldConstants.js";
+
+export default class RoadSegment {
+
+    constructor(z = 0) {
+
+        this.mesh = this.createSegment();
+
+        this.mesh.position.z = z;
+
+    }
+
+    createSegment() {
+
+        const geometry = new THREE.PlaneGeometry(
+            WORLD.ROAD_WIDTH,
+            WORLD.ROAD_LENGTH
+        );
+
+        const material = new THREE.MeshStandardMaterial({
+            color: 0x2d2d2d
+        });
+
+        const road = new THREE.Mesh(
+            geometry,
+            material
+        );
+
+        road.rotation.x = -Math.PI / 2;
+        road.receiveShadow = true;
+
+        return road;
+
+    }
+
+    update(speed) {
+
+        this.mesh.position.z += speed;
+
+    }
+
+    setZ(z) {
+
+        this.mesh.position.z = z;
+
+    }
+
+    getZ() {
+
+        return this.mesh.position.z;
+
+    }
+
+    getMesh() {
+
+        return this.mesh;
+
+    }
+
+}
