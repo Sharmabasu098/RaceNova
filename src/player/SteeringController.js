d3c347babee07b34f96ee207d4bf43f7e9f98c4a
Basudev sharma <147743632+Sharmabasu098@users.noreply.github.com>
2026-08-02 14:14:11 +0530

    Update SteeringController.js

--- src/player/SteeringController.js
+++ src/player/SteeringController.js
@@ -1,1 +1,51 @@
+/**
+ * ============================================================
+ * RaceNova
+ * Steering Controller
+ * Version : 1.0
+ * ============================================================
+ */
 
+export default class SteeringController {
+
+    constructor(player) {
+
+        this.player = player;
+
+        this.moveSpeed = 8;
+
+        this.minX = -4;
+
+        this.maxX = 4;
+
+    }
+
+    moveLeft(deltaTime) {
+
+        const mesh = this.player.getMesh();
+
+        mesh.position.x -= this.moveSpeed * deltaTime;
+
+        if (mesh.position.x < this.minX) {
+
+            mesh.position.x = this.minX;
+
+        }
+
+    }
+
+    moveRight(deltaTime) {
+
+        const mesh = this.player.getMesh();
+
+        mesh.position.x += this.moveSpeed * deltaTime;
+
+        if (mesh.position.x > this.maxX) {
+
+            mesh.position.x = this.maxX;
+
+        }
+
+    }
+
+}
