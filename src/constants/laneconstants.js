f1b501c5259c0ce567fdad870da88eeffc8da8f7
Basudev sharma <147743632+Sharmabasu098@users.noreply.github.com>
2026-08-04 07:50:26 +0530

    Update laneconstants.js

--- src/constants/laneconstants.js
+++ src/constants/laneconstants.js
@@ -1,1 +1,24 @@
+/**
+ * ============================================================
+ * RaceNova
+ * Lane Constants
+ * ============================================================
+ */
 
+export const LANES = {
+
+    LEFT: -4,
+
+    CENTER: 0,
+
+    RIGHT: 4
+
+};
+
+export const LANE_ORDER = [
+
+    LANES.LEFT,
+    LANES.CENTER,
+    LANES.RIGHT
+
+];
